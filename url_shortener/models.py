a0170445053bb62ec61eae7d8b4d7d3e3956014a
Vivek Kekuda <vivek@localhost.localdomain>
2020-12-02 01:05:41 +0530

    initial commit

---
+++ url_shortener/models.py
@@ -0,0 +1,40 @@
+import string
+from datetime import datetime
+from base64 import b64encode
+from hashlib import blake2b
+import random
+
+from .extensions import db 
+
+class Link(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    original_url = db.Column(db.String(512))
+    short_url = db.Column(db.String(12), unique=True)
+    visits = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.now)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.short_url = self.generate_short_link()
+
+    def generate_short_link(self):
+        """
+        Shortens a url by generating a 9 byte hash, and then
+        converting it to a 12 character long base 64 url friendly string.
+
+        Parameters:
+        url - the url to be shortened.
+
+        Return values:
+        String, the unique shortened url, acting as a key for the entered long url.
+        """
+        url_hash = blake2b(str.encode(self.original_url), digest_size=9)
+        b64 = b64encode(url_hash.digest(), altchars=b'-_').decode('utf-8')
+
+        already_shortened = self.query.filter_by(short_url=b64).first()
+
+        if already_shortened:
+            self.original_url += str(random.randint(0, 9))
+            return self.generate_short_link()
+        
+        return b64
---
+++ url_shortener/routes.py
@@ -0,0 +1,70 @@
+from flask import Blueprint, render_template, request, redirect
+
+from .extensions import db
+from .models import Link
+
+import re
+
+def url_valid(url):
+    """Validates a url by parsing it with a regular expression.
+
+    Parameters:
+    url - string representing a url to be validated.
+
+    Return values:
+    Boolean, indicating the validity of the url.
+    """
+
+    # From https://stackoverflow.com/questions/7160737/python-how-to-validate-a-url-in-python-malformed-or-not#7160778
+    # Slightly modified to not use ftp.
+    regex = re.compile(
+        r'^(?:http)s?://'
+        r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
+        r'localhost|'
+        r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'
+        r'(?::\d+)?'
+        r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+
+    return re.match(regex, url) is not None
+
+short = Blueprint('short', __name__)
+
+@short.route('/<short_url>')
+def redirect_to_url(short_url):
+    link = Link.query.filter_by(short_url=short_url).first_or_404()
+
+    link.visits = link.visits + 1
+    db.session.commit()
+
+    return redirect(link.original_url) 
+
+@short.route('/')
+def index():
+    return render_template('index.html')
+
+@short.route('/add_link', methods=['POST'])
+def add_link():
+    url = request.form['original_url']
+    
+    if url[:4] != 'http':
+        url = 'http://' + url
+
+    if not url_valid(url):
+        return render_template('invalid.html', url=url)
+
+    link = Link(original_url=url)
+    db.session.add(link)
+    db.session.commit()
+
+    return render_template('link_added.html', 
+        short_url=link.short_url, original_url=link.original_url)
+
+@short.route('/stats')
+def stats():
+    links = Link.query.all()
+
+    return render_template('stats.html', links=links)
+
+@short.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
